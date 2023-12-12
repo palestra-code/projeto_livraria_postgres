375c9e188ffcf8d413fe2a6c293464c0c19c76e2
palestra <153216715+palestra-code@users.noreply.github.com>
2023-12-12 23:27:09 +0000

    Update categoria_dao.py

--- dao/categoria_dao.py
+++ dao/categoria_dao.py
@@ -46,13 +46,19 @@ class CategoriaDAO:
         if categorias_removidas == 0:
             return False
         return True
-        
+
     def buscar_por_id(self, categoria_id) -> Categoria:
         cat = None
-        for c in self.__categorias:
-            if (c.id == categoria_id):
-                cat = c
-                break
+        conexao = self.__conexao_factory.get_conexao()
+        cursor = conexao.cursor()
+        cursor.execute("SELECT id, nome FROM categorias  WHERE id = %s", (categoria_id,))
+        resultado = cursor.fetchone()
+        if (resultado):
+            cat = Categoria(resultado[1])
+            cat.id = resultado[0]
+        cursor.close()
+        conexao.close()
         return cat
+
     
     
